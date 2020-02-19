0d106943658afbc8c3cd84157da913143f834b1a
xiang.hu胡祥 <hu.xiang@chinaredstar.com>
2020-02-19 20:03:27 +0800

    build: 新增打包构建
    1.新增gulp打包
    2.新增rollup打包
    3.新增tslint语法检查

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+const { series, src, dest } = require('gulp');
+const ts = require('gulp-typescript');
+const clean = require('gulp-clean');
+
+const rollup = require('rollup');
+const babel = require('rollup-plugin-babel');
+const commonjs = require('rollup-plugin-commonjs');
+const resolve = require('rollup-plugin-node-resolve');
+const json = require('@rollup/plugin-json');
+const { preserveShebangs } = require('rollup-plugin-preserve-shebangs');
+
+const { terser } = require('rollup-plugin-terser');
+
+const tsProject = ts.createProject('tsconfig.json', { emitDeclarationOnly: true, });
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+const babelConfig = {
+  presets: ['@babel/env', '@babel/typescript'],
+  plugins: ['@babel/transform-runtime', '@babel/proposal-class-properties'],
+};
+
+// 清空lib构建目录
+function cleanDistTask() {
+  return src(['lib'], { read: false, allowEmpty: true })
+    .pipe(clean({ force: true }));
+}
+
+// 清空Types目录
+function cleanTypesTask() {
+  return src(['types'], { read: false, allowEmpty: true })
+    .pipe(clean({ force: true }));
+}
+
+// 构建types
+function buildTypesTask() {
+  const tsResult = src("sources/*.ts")
+    .pipe(tsProject());
+  return tsResult.dts.pipe(dest('types'))
+}
+
+function rollupWithOption(inputOption, outOption) {
+  return rollup.rollup(inputOption).then(bundle => bundle.write(outOption));
+}
+
+
+// 构建umd包。可以扩展
+async function rollupTask() {
+
+  const inputOption = {
+    input: 'sources/index.ts',
+    external: ['commander', 'path', 'fs', 'chalk', 'ora', 'tpl_apply', 'mkdirp'],
+    plugins: [
+      preserveShebangs(),
+      json(),
+      // Allows node_modules resolution
+      resolve({ extensions }),
+      // Allow bundling cjs modules. Rollup doesn't understand cjs
+      commonjs(),
+      // Compile TypeScript/JavaScript files
+      babel({
+        ...babelConfig,
+        runtimeHelpers: true,
+        extensions,
+        exclude: 'node_modules/**',
+      }),
+      // Minify generated es bundle
+      terser(),
+    ]
+  };
+
+  await rollupWithOption(inputOption, { file: 'lib/index.js', format: 'cjs' });
+}
+
+
+exports.buildTypes = series([cleanTypesTask, buildTypesTask]);
+
+exports.default = series([cleanDistTask, rollupTask]);
